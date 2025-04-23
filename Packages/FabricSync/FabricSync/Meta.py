7f50f4c319f04a63d0ce52a6549d019952b62588
bluewatersql <chriprice@microsoft.com>
2025-04-23 12:33:18 -0400

    v17
    Meta Update

--- Packages/FabricSync/FabricSync/Meta.py
+++ Packages/FabricSync/FabricSync/Meta.py
@@ -3,4 +3,4 @@ warnings.filterwarnings("ignore", category=UserWarning)
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 class Version():
-    CurrentVersion = "2.1.16"
+    CurrentVersion = "2.1.17"
